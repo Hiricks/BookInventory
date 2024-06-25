cfca9860ebbcf23a229f4a6d456c5fabc7d12808
Hiricks <44841708+Hiricks@users.noreply.github.com>
2024-06-25 16:52:15 +0000

    Add project files.

---
+++ BookInventory/Shared/Services/BookService.cs
@@ -0,0 +1,48 @@
+﻿using BookInventory.Models;
+
+
+namespace BookInventory.Shared.Services
+{
+    public class BookService
+    {
+        private readonly List<Book> books = new List<Book>();
+
+        private int nextId = 1;
+
+        public IEnumerable<Book> GetBooks()
+        {
+            return books;
+        }
+
+        public Book GetBook(int id)
+        {
+            var book = books.FirstOrDefault(b => b.Id == id) ?? throw new ArgumentException("Book not found with the specified ID.");
+            return book;
+        }
+
+        public void AddBook(Book book)
+        {
+            book.Id = nextId++;
+            books.Add(book);
+        }
+        public void UpdateBook(Book book)
+        {
+            var existingBook = books.FirstOrDefault(b => b.Id == book.Id);
+            if (existingBook != null)
+            {
+                existingBook.Title = book.Title;
+                existingBook.Author = book.Author;
+                existingBook.Genre = book.Genre;
+                existingBook.Year = book.Year;
+            }
+        }
+        public void DeleteBook(int id)
+        {
+            var book = books.FirstOrDefault(b => b.Id == id);
+            if (book != null)
+            {
+                books.Remove(book);
+            }
+        }
+    }
+}
